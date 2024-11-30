c98ce3e87f525cc1bf5137336e0643da6383c56c
AlexSemin-cjder <119964665+AlexSemin-cjder@users.noreply.github.com>
2024-11-30 22:48:09 +0500

    "Рекурсия"

--- module_3_5.py
+++ module_3_5.py
@@ -7,6 +7,7 @@ def get_multiplied_digits(number):
         return int(str_number)
     elif len(str_number) > 1:
 
+
         return first * get_multiplied_digits(int(str_number[1:]))
 
 result = get_multiplied_digits(40203)
